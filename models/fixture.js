49a94b017ffc9e7e66082456bc6edde7e06c9c94
netaro-N <takebbb1+github@gmail.com>
2019-02-18 06:47:20 +0000

    data-modeling

--- models/fixture.js
+++ models/fixture.js
@@ -1,1 +1,41 @@
 // fixtureId,fixtureDate,fixtureSort,Home Team,Away Team,homeScore,awayScore
+'use strict';
+const loader = require('./sequelize-loader');
+const Sequelize = loader.Sequelize;
+
+const Fixture = loader.database.define('fixtures', {
+  fixtureId: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    allowNull: false
+  },
+  fixtureDate: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  fixtureSort: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  homeTeam:{
+    type: Sequelize.INTEGER,
+    allowNull:false
+  },
+  awayTeam: {
+    type: Sequelize.INTEGER,
+    allowNull:false
+  },
+  homeScore: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  awayScore: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+}, {
+    freezeTableName: true,
+    timestamps: false
+  });
+
+module.exports = Fixture;
