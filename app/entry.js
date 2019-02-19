ac08d63afaa7e143aa34d98cc7486bd9d203cf5d
netaro-N <takebbb1+github@gmail.com>
2019-02-19 02:19:25 +0000

    first csv-parse

--- app/entry.js
+++ app/entry.js
@@ -3,7 +3,7 @@ import $ from 'jQuery';
 
 const buttonCsvParse = $('#csv-parse-button');
 buttonCsvParse.click(() => {
-  const confirm = confirm('サーバーにあるcsvファイルをデータベースに保存しますか？');
+  const confirm = window.confirm('サーバーにあるcsvファイルをデータベースに保存しますか？');
   if(confirm){
     console.log('保存を実行します');
   };
