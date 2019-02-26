e1ff78c27420f1cd0d193e8dae44b0a26c688880
netaro-N <takebbb1+github@gmail.com>
2019-02-26 06:37:21 +0000

    catch data

--- routes/newEdit.js
+++ routes/newEdit.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+router.post('/new', (req, res, next) => {
+  console.log(req.body); // TODO 予定と候補を保存する実装をする
+  res.redirect('/new');
+});
 
 module.exports = router;
