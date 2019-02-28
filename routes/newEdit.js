cf83e492de7c0a3a6397410f7c972e16ee106b3f
netaro-N <takebbb1+github@gmail.com>
2019-02-28 02:45:17 +0000

    ready for save

--- routes/newEdit.js
+++ routes/newEdit.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment-timezone');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const Fixture = require('../models/fixture');
 
 router.post('/new', (req, res, next) => {
   const fixtureDate = new Date(req.body.fixtureDate);
-  console.log(fixtureDate);
+  const formattedDate = moment(fixtureDate).format("YYYY/MM/DD HH:mm");
+  console.log(fixtureDate + "→" + formattedDate);
   console.log(req.body);
   res.redirect('/new');
 });
